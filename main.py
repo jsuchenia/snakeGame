08baabf95fcfda8c55c6590c2d8831fbbcfc54ed
Jacek Suchenia <jacek.suchenia@gmail.com>
2022-02-20 12:36:31 +0100

    Update stefan game

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
 from random import randrange, choice
 import pygame
 import sys
+import torch
 
 GRID_SIZE = 32
 CELL_SIZE = 15
+SCREEN_SIZE = GRID_SIZE * CELL_SIZE
 
 UP = (0, -1)
 DOWN = (0, 1)
@@ -15,6 +20,8 @@ DIRECTIONS = [UP, RIGHT, DOWN, LEFT]
 
 class SnakeGame:
     def __init__(self, positions=None, direction=None, food=None):
+        self._lives = 0
+
         if positions and direction and food:
             self._positions = positions
             self._direction = direction
@@ -25,6 +32,7 @@ class SnakeGame:
     def reset(self):
         self._positions = [(randrange(GRID_SIZE), randrange(GRID_SIZE))]
         self._direction = choice(DIRECTIONS)
+        self._lives += 1
         self.newfood()
 
     def move(self, direction=None):
@@ -84,18 +92,30 @@ class SnakeGame:
         if abs(cur-new) != 2:
             self._direction = direction
 
+    @property
+    def lives(self):
+        return self._lives
+
 
 if __name__ == '__main__':
     snake = SnakeGame()
 
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using {device} device")
+
     pygame.init()
-    SCREEN_SIZE = GRID_SIZE*CELL_SIZE
-    screen = pygame.display.set_mode([SCREEN_SIZE, SCREEN_SIZE])
-    gameSurface = pygame.Surface((SCREEN_SIZE, SCREEN_SIZE))
+    pygame.display.set_caption("Snake Game - Stefan")
+    screen = pygame.display.set_mode([SCREEN_SIZE, SCREEN_SIZE+100])
+    gameSurface = pygame.Surface((SCREEN_SIZE, SCREEN_SIZE+100))
+
+    font = pygame.font.SysFont('Arial', 25)
+
     clock = pygame.time.Clock()
 
+    maxscore = 0
+
     while True:
-        clock.tick(10)
+        clock.tick(5)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -115,18 +135,22 @@ if __name__ == '__main__':
         for y in range(GRID_SIZE):
             for x in range(GRID_SIZE):
                 if ((x + y) % 2) == 0:
-                    pygame.draw.rect(gameSurface, (0, 200, 0),
+                    pygame.draw.rect(gameSurface, (0, 190, 0),
                                      pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
                 else:
-                    pygame.draw.rect(gameSurface, (0, 180, 0),
+                    pygame.draw.rect(gameSurface, (0, 170, 0),
                                      pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
         for s in snake.positions:
             pygame.draw.rect(gameSurface, (0, 0, 200), pygame.Rect((s[0] * CELL_SIZE, s[1] * CELL_SIZE), (CELL_SIZE, CELL_SIZE)))
 
         food = snake.food
+        maxscore = max(maxscore, snake.score)
         pygame.draw.rect(gameSurface, (250, 0, 0), pygame.Rect((food[0] * CELL_SIZE, food[1] * CELL_SIZE), (CELL_SIZE, CELL_SIZE)))
-
+        pygame.draw.rect(gameSurface, (0, 0, 0), pygame.Rect((0, SCREEN_SIZE), (SCREEN_SIZE, 100)))
+        gameSurface.blit(font.render(f"Points: {snake.score}", False, (255, 255, 255), (0, 0, 0)), (5, SCREEN_SIZE))
+        gameSurface.blit(font.render(f"Lives: {snake.lives}", False, (255, 255, 255), (0, 0, 0)), (SCREEN_SIZE - 110, SCREEN_SIZE))
+        gameSurface.blit(font.render(f"Max: {maxscore}", False, (255, 255, 255), (0, 0, 0)), (5, SCREEN_SIZE + 30))
         screen.blit(gameSurface, pygame.Rect(0, 0, SCREEN_SIZE, SCREEN_SIZE))
         pygame.display.flip()
 
